e63993f47fc0e08fec9784ba77cde31b7e3d42ff
rosandrest <rosandrest@pm.me>
2021-08-10 14:32:52 -0700

    activated missing files

--- src/pages/bg-image.js
+++ src/pages/bg-image.js
@@ -1,29 +1,29 @@
-// import React from 'react'
-// import { graphql, useStaticQuery } from 'gatsby'
-// import {getImage} from 'gatsby-plugin-image'
-// import {BgImage} from 'gbimage-bridge'
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import {getImage} from 'gatsby-plugin-image'
+import {BgImage} from 'gbimage-bridge'
 
 
-// const GbiBridged = () => {
-//     const {backgroundCover}= useStaticQuery(
-//         graphql`
-//             query {
-//                 backgroundCover: file(relativePath: {eq: "cover.jpg"}) {
-//                     childImageSharp {
-//                     gatsbyImageData(width: 2000, webpOptions: {quality: 70}, quality: 50)
-//                     }
-//                     }
-//                 }
-//             `
-//         )
+const GbiBridged = () => {
+    const {backgroundCover}= useStaticQuery(
+        graphql`
+            query {
+                backgroundCover: file(relativePath: {eq: "cover.jpg"}) {
+                    childImageSharp {
+                    gatsbyImageData(width: 2000, webpOptions: {quality: 70}, quality: 50)
+                    }
+                    }
+                }
+            `
+        )
 
 
-//     const pluginImage = getImage(backgroundCover)
+    const pluginImage = getImage(backgroundCover)
     
 
-//     return (
-//        <BgImage image={pluginImage}>Test</BgImage>
-//         )
-// }
+    return (
+       <BgImage image={pluginImage}>Test</BgImage>
+        )
+}
 
-// export default GbiBridged
+export default GbiBridged
