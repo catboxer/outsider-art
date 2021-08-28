fa3f08fc176df6ff0f1ae6354bae7d5d511d1887
rosandrest <rosandrest@pm.me>
2021-08-28 10:48:30 -0400

    change placeholder

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -10,7 +10,11 @@ const query = graphql`
         cover_img {
           localFiles {
             childImageSharp {
-              gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED, height: 104)
+              gatsbyImageData(
+                placeholder: BLURRED
+                layout: CONSTRAINED
+                height: 104
+              )
             }
           }
         }
@@ -51,74 +55,5 @@ const Slider = () => {
         )
   }
 
-// const Wrapper = styled.div`
-//   background: var(--clr-white);
-//   max-width: 50vw;
-//   .image-container {
-//     width:109px;
-//     min-width:109px;
-//     height: 109px;
-//     border: white 6px solid;
-//     box-shadow: 0 0.2rem 1.2rem rgba(0,0,0,0.2);
-//     border-radius: var(--radius);
-//     transition: var(--transition);
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     overflow: hidden
-//   }
-//     .thumbnail {
-//     min-width: 100%;
-//     min-height: 100%;
-      
-//     }
-//     .next{
-//       width:10px;
-//     }
-//     article {
-//       border: #ccc solid 1px;
-//       border-top: none;
-//       display: flex;
-//       /* box-shadow: var(--light-shadow);
-//       border-radius: var(--radius); */
-//       transition: var(--transition);
-//     }
-//     article:hover {
-//       box-shadow: var(--dark-shadow);
-//     }
-//     .about-blocks{
-//       border: yellow 2px solid;
-//       margin-left: 1rem;
-//     }
-//     .container {
-//       display: flex;
-//       border: red solid 2px;
-//       width: 50vw;
-//       max-height: 14rem;
-//       padding: .5rem;
-//       /* overflow: hidden; */
-//       background: var(--clr-white);
-//       &:hover .img {
-//         opacity: 0.2;
-//       }
-      
-//     @media (min-width: 768px) {
-//       .container {
-//         height: 15rem;
-//       }
-//       grid-template-columns: 1fr 1fr;
-//     }
-//     @media (min-width: 992px) {
-//       .container {
-//         height: 12.5rem;
-//       }
-//       grid-template-columns: 1fr 1fr 1fr;
-//     }
-//     @media (min-width: 1200px) {
-//       .container {
-//         height: 15rem;
-//       }
-//     }
-// `
 
 export default Slider
